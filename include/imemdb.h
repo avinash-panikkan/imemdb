8b1bb7caf2724c2f7a434bdde53da6da9f3df39b
avinash-panikkan <avinkb11@gmail.com>
2025-09-24 22:56:36 +0530

    basic-kv-store   - created KeyValueStore class implemented the basic put, get and remove functionalities.   - created a test for the implemented code.

---
+++ include/imemdb.h
@@ -0,0 +1,6 @@
+#ifndef IMEMDB_H
+#define IMEMDB_H
+
+#include "imemdb/kv_store.h"
+
+#endif // IMEMDB_H
---
+++ src/kv_store.cpp
@@ -0,0 +1,27 @@
+#include "imemdb/kv_store.h"
+
+namespace imemdb {
+
+void KeyValueStore::put(const std::string &key, const std::string &value)
+{
+    std::unique_lock lock(m_mutex);
+    m_store[key] = value;
+}
+
+std::optional<std::string> KeyValueStore::get(const std::string &key) const
+{
+    std::shared_lock lock(m_mutex);
+    auto iter = m_store.find(key);
+    if (iter != m_store.end())
+        return iter->second;
+
+    return std::nullopt;
+}
+
+bool KeyValueStore::remove(const std::string &key)
+{
+    std::unique_lock lock(m_mutex);
+    return m_store.erase(key) > 0;
+}
+
+} // namespace imemdb
